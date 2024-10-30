6e995360bbf1b182e00cd789ee2350785b6be393
Ryszard Mochalski <138623138+rysz99@users.noreply.github.com>
2024-10-30 21:26:01 +0100

    Update wisielec.js

--- wisielec.js
+++ wisielec.js
@@ -120,7 +120,7 @@ function sprawdz(nr)
             document.getElementById(element).setAttribute("onclick",";");
         
         ile_skuch++;
-        var obraz = "img/s"+ile_skuch+".jpg";
+        var obraz = "s"+ile_skuch+".jpg";
         document.getElementById("szubienica").innerHTML = '<img src="'+obraz+'" alt="" />';
     }
     
